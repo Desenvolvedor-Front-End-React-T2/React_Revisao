a57bd4b910de1fd5cc387b9f222b3162c3e255ee
Alvaro Pimentel <alvarohcpimentel@gmail.com>
2026-07-31 22:29:53 -0300

    Feature(SCTech): Tela inicial com Form sem estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,58 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import InputTest from './components/inputTest/index.jsx'
 import './App.css'
 
 function App() {
   // const [count, setCount] = useState(0)
-  const [estado, setEstado] = useState('')
+  // const [state, setState] = useState(
+  //   {
+  //     'Usuário': '',
+  //     'Senha': ''
+  //   }
+  // )
+
+  // const [login, setLogin] = useState(false)
+  // const [title, setTitle] = useState('Tela de login...')
+
+  // useEffect(() => {
+  //   if (login) {
+  //     // JSON.parse(localStorage.getItem('Credentials'))[0]
+  //   }
+  // }, [login])
+
+  const [user, setUser] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = (loginData) => {
+    loginData.preventDefault()
+    const credentials = JSON.parse(localStorage.getItem('Credentials'))
+    if (loginData.target[0].value === credentials[0] && loginData.target[1].value === credentials[1]) {
+      setLogin(true)
+      alert('Login realizado com sucesso!')
+    } else {
+      alert('Usuário ou senha incorretos!')
+    }
+  }
 
   return (
     <>
+      {localStorage.setItem('Credentials', JSON.stringify(['sctech', '123']))}
+      { }
       <h1>
-        Oi!
+        {/* {title} */}
       </h1>
-      <div className='g-4' style={{ display: 'flex', flexDirection: 'column' }}>
-        <InputTest estado={estado} setEstado={setEstado} />
-        <InputTest estado={estado} setEstado={setEstado} />
-      </div>
+      <h2>Tela de login...</h2>
+      <p>Digite abaixo seus dados.</p>
+
+      <form onSubmit={handleSubmit}>
+        <div className='g-4' style={{ display: 'flex', flexDirection: 'column' }}>
+          <InputTest name={'Usuário'} state={user} setState={setUser} />
+          <InputTest name={'Senha'} state={password} setState={setPassword} />
+        </div>
+        <button type='submit' >Entrar</button>
+      </form>
+
+      {/* {console.log(user + ' - ' + password)} */}
     </>
   )
 }
--- src/components/inputTest/index.jsx
+++ src/components/inputTest/index.jsx
@@ -1,11 +1,12 @@
 
-function InputTest({ estado, setEstado }) {
+function InputTest({ name, state, setState }) {
 
     return (
         <>
+            <label>{name || 'Campo a ser preenchido'}</label>
             <input
-                value={estado}
-                onChange={(e) => setEstado(e.target.value)}
+                value={state}
+                onChange={(e) => setState(e.target.value)}
                 style={{ width: '150px' }}
             />
             {/* <p>{estado || 'Digite algo...'}</p> */}
